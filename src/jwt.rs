bd2bddd0b4523d61cdf9d9611160292b95559cf0
Xorycode <xorycode@shuriken.pm>
2024-06-01 22:13:15 +0300

    fix: jwt

--- src/jwt.rs
+++ src/jwt.rs
@@ -1,13 +1,23 @@
 use serde::{Serialize, Deserialize};
 use jsonwebtoken::{encode, decode, Header, Algorithm, Validation, EncodingKey, DecodingKey, errors::Result};
+use std::time::{SystemTime, Duration, UNIX_EPOCH};
 
 const SECRET_KEY: &[u8] = b"temp";
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Claims { pub id: String }
+pub struct Claims { pub id: String, pub exp: usize }
+
+fn calculate_jwt_expiry() -> usize {
+    let now = SystemTime::now();
+    let thirty_days = Duration::from_secs(30 * 24 * 60 * 60);
+    let expiry_date = now.checked_add(thirty_days);
+    expiry_date.unwrap().duration_since(UNIX_EPOCH).unwrap().as_secs().try_into().unwrap() 
+    // Literally no reason for this to fail (famous last words)
+}
 
 pub fn create_jwt(id: &str) -> Result<String> {
-    let claims = Claims { id: id.to_owned() };
+    let exp = calculate_jwt_expiry();
+    let claims = Claims { id: id.to_owned(), exp: exp };
         encode(&Header::default(), &claims, &EncodingKey::from_secret(SECRET_KEY))
     }
 
