add1d1a2598a31fab8290598b7c96e381cbf704d
Xorycode <xorycode@shuriken.pm>
2024-06-01 23:52:13 +0300

    fix: jwt secret key is now read from .env

--- src/jwt.rs
+++ src/jwt.rs
@@ -1,12 +1,14 @@
 use serde::{Serialize, Deserialize};
 use jsonwebtoken::{encode, decode, Header, Algorithm, Validation, EncodingKey, DecodingKey, errors::Result};
 use std::time::{SystemTime, Duration, UNIX_EPOCH};
+use std::fs::read_to_string;
 
-const SECRET_KEY: &[u8] = b"temp";
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Claims { pub id: String, pub exp: usize }
 
+
+// Calculate the JWT Expiry date (30 days from generation)
 fn calculate_jwt_expiry() -> usize {
     let now = SystemTime::now();
     let thirty_days = Duration::from_secs(30 * 24 * 60 * 60);
@@ -15,15 +17,28 @@ fn calculate_jwt_expiry() -> usize {
     // Literally no reason for this to fail (famous last words)
 }
 
+fn get_secret_key() -> Vec<u8> {
+    let envfile = read_to_string(".env").expect("Could not find .env");
+    let secret_key: &[u8] = envfile
+        .lines()
+        .filter(|line| line.starts_with("JWT_SECRET_KEY="))
+        .next()
+        .expect("Could not find JWT_SECRET_KEY in .env")
+        .as_bytes();
+    secret_key.to_vec()
+}
+
 pub fn create_jwt(id: &str) -> Result<String> {
     let exp = calculate_jwt_expiry();
     let claims = Claims { id: id.to_owned(), exp: exp };
-        encode(&Header::default(), &claims, &EncodingKey::from_secret(SECRET_KEY))
-    }
+    let secret_key = get_secret_key();
+    encode(&Header::default(), &claims, &EncodingKey::from_secret(secret_key.as_slice()))
+}
 
 pub fn validate_jwt(token: &str) -> Result<Claims> {
     let validation = Validation::new(Algorithm::HS256);
-    let token_data = decode::<Claims>(token, &DecodingKey::from_secret(SECRET_KEY), &validation)?;
+    let secret_key = get_secret_key();
+    let token_data = decode::<Claims>(token, &DecodingKey::from_secret(secret_key.as_slice()), &validation)?;
     Ok(token_data.claims)
 }
 
